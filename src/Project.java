ae9a32662d82e5b139fd47853ad74e0c44f8d456
markpang <1800591@sit.singaporetech.edu.sg>
2020-02-11 17:54:12 +0800

    temp

--- src/Project.java
+++ src/Project.java
@@ -3,23 +3,39 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.List;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.JScrollBar;
-import javax.swing.JScrollPane;
 
-public class Project extends JFrame {
+public class Project extends JFrame implements UnderlyingActivityListener {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField tfName;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private JTextArea onlineUserTextArea;
+	private JTextArea groupsTextArea;
+	private JTextArea conversationTextArea;
+
+
+	public UnderlyingService mainBroadcastService;
+	public String userName;
+	public List<String> onlineUsers;
+	public List<String> selectedGroups;
+
+//	public UnderlyingReplyListener listener = new UnderlyingReplyListener() {
+//		@Override
+//		public void onReply(List<String> args) {
+//
+//		}
+//
+//		@Override
+//		public void onTimeout() {
+//
+//		}
+//	};
 
 	/**
 	 * Launch the application.
@@ -36,6 +52,22 @@ public class Project extends JFrame {
 			}
 		});
 	}
+
+
+
+	public UnderlyingService getInstance() {
+		UnderlyingService broadcastService = null;
+		try {
+			broadcastService = new UnderlyingService(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return broadcastService;
+	}
+
+
+
+
 
 	/**
 	 * Create the frame.
@@ -54,10 +86,10 @@ public class Project extends JFrame {
 		btnRegisterUser.setBounds(10, 11, 112, 23);
 		contentPane.add(btnRegisterUser);
 		
-		textField = new JTextField();
-		textField.setBounds(132, 12, 225, 22);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		tfName = new JTextField();
+		tfName.setBounds(132, 12, 225, 22);
+		contentPane.add(tfName);
+		tfName.setColumns(10);
 		
 		JLabel lblGroupManagement = new JLabel("Group Management");
 		lblGroupManagement.setForeground(Color.WHITE);
@@ -81,27 +113,27 @@ public class Project extends JFrame {
 		lblOnlineUser.setBounds(10, 135, 98, 14);
 		contentPane.add(lblOnlineUser);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(288, 160, 383, 258);
-		contentPane.add(textArea);
+		onlineUserTextArea = new JTextArea();
+		onlineUserTextArea.setBounds(288, 160, 383, 258);
+		contentPane.add(onlineUserTextArea);
 		
 		JLabel lblNewLabel = new JLabel("Groups");
 		lblNewLabel.setForeground(Color.WHITE);
 		lblNewLabel.setBounds(145, 135, 133, 14);
 		contentPane.add(lblNewLabel);
 		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setBounds(145, 160, 133, 258);
-		contentPane.add(textArea_1);
+		groupsTextArea = new JTextArea();
+		groupsTextArea.setBounds(145, 160, 133, 258);
+		contentPane.add(groupsTextArea);
 			
 		JLabel lblConversation = new JLabel("Conversation");
 		lblConversation.setForeground(Color.WHITE);
 		lblConversation.setBounds(288, 135, 243, 14);
 		contentPane.add(lblConversation);
 		
-		JTextArea textArea_2 = new JTextArea();
-		textArea_2.setBounds(10, 160, 125, 258);
-		contentPane.add(textArea_2);
+		conversationTextArea = new JTextArea();
+		conversationTextArea.setBounds(10, 160, 125, 258);
+		contentPane.add(conversationTextArea);
 		
 		JButton btnSendMessage = new JButton("Send Message");
 		btnSendMessage.setBounds(10, 429, 121, 23);
@@ -116,7 +148,50 @@ public class Project extends JFrame {
 		textField_2.setBounds(10, 70, 267, 20);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
-		
+
+
+
+		mainBroadcastService = getInstance();
+
+
+
+
+			//Get all online users
+
+		btnRegisterUser.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				String tempName = tfName.getText();
+
+				if (tempName.equals("") || tempName.substring(0, 1).matches("[0-9]") || tempName.length() > 8) {
+
+					JOptionPane.showMessageDialog(new JFrame(), "Username invalid", "Error", JOptionPane.ERROR_MESSAGE);
+				}else {
+
+					mainBroadcastService.checkExistingUser(tempName, new UnderlyingReplyListener() {
+						@Override
+						public void onReply(List<String> args) {
+
+							JOptionPane.showMessageDialog(new JFrame(), "Username taken", "Error", JOptionPane.ERROR_MESSAGE);
+						}
+						@Override
+						public void onTimeout() {
+							userName = tempName;
+							mainBroadcastService.broadcastUsername(userName);
+							mainBroadcastService.currentUsername = userName;
+							System.out.println(userName);
+						}
+					});
+//					onUserOnline(userName);
+
+				}
+
+			}
+		});
+
+
+
 		JButton btnProfile = new JButton("Profile");
 		btnProfile.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -129,5 +204,83 @@ public class Project extends JFrame {
 		});
 		btnProfile.setBounds(367, 11, 89, 23);
 		contentPane.add(btnProfile);
+
+
+
+
+
+
+
+
+
+
+
+
+
 	}
+
+
+
+
+
+	public void clearChat(){
+		conversationTextArea.setText("");
+	}
+
+
+	// set Chat button to be true
+	public void enableChatButton(){
+
+	}
+
+	public void disableChat(){
+
+	}
+
+
+	public void renderOnlineUsers(List<String> onlineUsers){
+		for (int i = 0 ; i < onlineUsers.size(); i++) {
+			onlineUserTextArea.append(onlineUsers.get(i) + "\n");
+		}
+	}
+
+
+
+
+	@Override
+	public void onUserOnline(String username) {
+		if (!onlineUsers.contains(username)){
+			onlineUsers.add(username);
+			onlineUserTextArea.append(username + "\n");
+		}
+	}
+
+
+
+	@Override
+	public void onUserOffline(String username) {
+
+		if (onlineUsers.contains(username)){
+			onlineUsers.remove(username);
+			renderOnlineUsers(onlineUsers);
+		}
+	}
+
+
+	@Override
+	public void onJoinGroup(String groupName, String ip) {
+
+	}
+
+	@Override
+	public List<GroupMessage> onRequestLatestMessages(String groupName) {
+		return null;
+	}
+
+	@Override
+	public void onRequestLatestMessageResult(List<GroupMessage> allMessages) {
+
+
+	}
+
 }
